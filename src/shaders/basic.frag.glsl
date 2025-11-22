fe5f26dfedeae42ab6c633972d2dbc9de9a1f5c4
Aubrey <valara.official@gmail.com>
2025-11-21 23:53:29 -0800

    updated frag shader

--- src/shaders/basic.frag.glsl
+++ src/shaders/basic.frag.glsl
@@ -1,6 +1,14 @@
 #version 460 core
 out vec4 color;
 
+uniform float iTime;
+
 void main(){
-	color = vec4(1.0, 0.0, 0.0, 1.0);
+	float t = iTime + 10.0;
+
+	float r = abs(sin(t * 0.3));
+	float g = abs(sin(t * 0.6));
+	float b = abs(sin(t * 1.0));
+
+	color = vec4(r, g, b, 1.0);
 }
